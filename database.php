7b60f54f96a3e9db2c06361a634c6dfb89d3e5c3
rorosana <r.a.caraballo8@gmail.com>
2022-12-09 17:18:42 +0100

    Category and product tables have been created in jewel database.

--- database.php
+++ database.php
@@ -1,16 +1,42 @@
 <?php
-//Variables de conexión a la database.
 $server = 'localhost';
 $username = 'root';
 $password = '';
 $database = 'jewel';
 
-function conectar(){
-	$con = mysqli_connect($GLOBALS["server"], $GLOBALS["username"], $GLOBALS["password"], $GLOBALS["database"]) 
-    or die("Error al conectar con la base de datos");
-	return $con;
+
+// Create connection MySQLi Object-oriented.
+//https://www.w3schools.com/php/php_mysql_create.asp
+
+$conn = new mysqli($server, $username, $password, $database);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
 
+// Create database
+$sql = "CREATE DATABASE IF NOT EXISTS jewel";
+if ($conn->query($sql) === TRUE) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . $conn->error;
+}
 
+//Create category table
+$sql = "CREATE TABLE IF NOT EXISTS category(id_category int primary key auto_increment, name_category varchar(50))";
+if ($conn->query($sql) === TRUE) {
+  echo "Table created successfully";
+} else {
+  echo "Error creating table: " . $conn->error;
+}
 
+//Create product table
+$sql = "CREATE TABLE IF NOT EXISTS product(id_product int primary key auto_increment, name_product varchar(50), category int, foreign key (category) references category(id_category));";
+if ($conn->query($sql) === TRUE) {
+  echo "Table created successfully";
+} else {
+  echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
 ?>
