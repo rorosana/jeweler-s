2ce2e48412145ab7efd00101cf5acf451bafe5e5
rorosana <r.a.caraballo8@gmail.com>
2022-12-19 09:06:05 +0100

    styles.css done

--- client.php
+++ client.php
@@ -9,39 +9,56 @@ $client = new SoapClient("http://localhost/devs_rosa/act05/jeweler-s/server.php?
   <head>
     <meta charset="utf-8">
     <title>Jeweler's</title>
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"><link href="https://fonts.googleapis.com/css2?family=Mr+De+Haviland&family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Mr+De+Haviland&family=Roboto:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
 
     <h1>Jeweler's</h1>
+    <hr>
+    <p>
+      Con el siguiente formulario puedes insertar un producto en la base de datos.
+    </p>
+    <hr>
 
     <form action="#" method="POST">
       <input name="name_product" type="text" placeholder="Escribe el nombre del producto">
-      <input name="category" type="number" placeholder="Indica la categoría del producto">
-      <input name="submit" type="submit" value="Submit">
+      <p>Las categorías disponibles son: </br>
+        <b>1</b> - Earrings &nbsp;  <b>2</b> - Necklaces &nbsp;  <b>3</b> - Bracelets &nbsp; <b>4</b> - Rings
+      </p>
+      <div class="quantity">
+      <label> Indica la categoría:</label>
+      <input name="category" type="number" min="1" max="4"placeholder="">
+      </div>
+</br><input name="submit" type="submit" value="Submit">
+    <hr>
     </form>
 
 
 <?php
-
+//Parte del cliente para ejecutar la función de insertar producto cuando se envíe el formulario.
 if(isset($_POST['submit'])){
     $name_product = $_POST['name_product'] ?? '';
     $category = $_POST['category'] ?? '';
     $result = $client->insertProduct($name_product, $category);
 }
 
-$result = $client->categoryList();
-print_r($result);
-echo "<br/>";
+
 ?>
 
 <form action="#" method="POST">
-      <input name="category" type="number" placeholder="Indica la categoría del producto">
-      <input name="submit" type="submit" value="Submit">
+      <p>Tabla que muestra los productos clasificados por categoría:</p>
+      <hr>
+      <div class="quantity">
+      <label> Indica la categoría:</label>
+      <input name="category" type="number" min="1" max="4"placeholder="">
+      </div>
+</br><input name="submit" type="submit" value="Submit">
     </form>
 <?php 
-
+//Parte del cliente para ejecutar la función listar productos según la categoría que indica el user.
 if(isset($_POST['submit'])){
     $category = $_POST['category'] ?? '';
     $result = $client->get_products($category);
@@ -52,6 +69,16 @@ if(isset($_POST['submit'])){
       echo "</tr>";
     }
 }
+echo "<hr>";
+//Parte del cliente para ejecutar la función que lista las categorías.
+$result = $client->categoryList();
+echo "<table border='1'>";
+    foreach($result as $cat){
+      echo "<tr>";
+      echo "<td>$cat->id_category</td>";
+      echo "<td>$cat->name_category</td>";
+      echo "</tr>";
+    }
 ?>
 </body>
 
